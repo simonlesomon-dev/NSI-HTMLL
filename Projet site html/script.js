2aba82627302fbcb2dd83ba1ae69e994d417c0f7
Endermanslave <167208274+Endermanslave@users.noreply.github.com>
2024-04-29 18:03:30 +0200

    Update script.js

--- Projet site html/script.js
+++ Projet site html/script.js
@@ -6,7 +6,7 @@ function Affichage_velo(selectedValue) {
         imageDestination = 'elec.jpg';
 	document.getElementById("description").innerHTML ="Un vélo électrique est un cycle doté de pédales et d'un moteur électrique auxiliaire et embarquant une source d'énergie, généralement une batterie rechargeable. L'essor de ce produit est récent et certains concepts peuvent varier selon les législations régionales";
     } else if (selectedValue === 'vtt') {
-        imageDestination = 'vtt2.jpg';
+        imageDestination = 'vtt.jpg';
 	document.getElementById("description").innerHTML = "Le vélo tout-terrain, souvent abrégé en VTT, parfois appelé vélo de montagne par traduction de l'anglais mountain bike, est un vélo destiné à une utilisation sur terrain accidenté, hors des routes goudronnées. Il sert pour diverses activités de loisirs individuelles et collectives ainsi que pour des pratiques sportives réglementées par l'Union cycliste internationale. Les pratiquants peuvent être nommés « vététistes », « enduristes » ou « pilotes » en fonction du type de pratique";
     } else if (selectedValue === 'velo_de_ville') {
         imageDestination = 'velo_de_ville.jpg';
